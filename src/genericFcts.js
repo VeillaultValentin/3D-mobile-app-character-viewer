609eaa13f45bea02566135fee75cc188131a324c
VeillaultValentin <rrathalio@gmail.com>
2025-09-05 15:21:06 +0200

    Update genericFcts.js

--- src/genericFcts.js
+++ src/genericFcts.js
@@ -19,3 +19,18 @@ export function trackPointer(event, pointerVector) {
     pointerVector.x = (event.clientX / window.innerWidth) * 2 - 1;
     pointerVector.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
+
+export function loadJSON(url) {
+    return new Promise((resolve, reject) => {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = (data) => {
+            resolve(JSON.parse(data.currentTarget.response));
+        }
+        xhttp.open('GET', url, false);
+        xhttp.send();
+        xhttp.onerror = () => {
+            console.log("Error: Could not load JSON at '"+ url +"'");
+            reject(false);
+        }
+    });
+}
